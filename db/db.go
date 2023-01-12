c8fa40963d812d8d0e2668b522f3385854f89826
orderzi <orderzi98@gmail.com>
2023-01-12 19:31:15 +0200

    Adding DB connect

--- db/db.go
+++ db/db.go
@@ -1,20 +1,40 @@
 package db
 
 import (
+	"database/sql"
+	"fmt"
 	"os"
 
-	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/orderzi/workout-service/types"
 )
 
-// var host string = os.Getenv("DB_HOST")
-// var User string = os.Getenv("DB_USER")
-// var Password string = os.Getenv("DB_PASSWORD")
-// var Port int = os.Getenv("DB_PORT")
-// var DBName string = os.Getenv("DB_NAME")
+func (c *types.DatabaseConnection) OpenDBSession() (*sql.DB, error) {
+	if c.User == "" {
+		c.User = "root"
+	}
+	if c.Password == "" {
+		c.Password = "password"
+	}
+	if c.Host == "" {
+		c.Host = "localhost"
+	}
+	if c.Port == "" {
+		c.Port = "3306"
+	}
+	if c.DBName == "" {
+		c.DBName = "users"
+	}
 
-
-
-func OpenDbSession(dbconfig types.DatabaseConnection){
-	sql , err := mysql.
+	c.User = os.Getenv("DB_USER")
+	c.Password = os.Getenv("DB_PASSWORD")
+	c.Host = os.Getenv("DB_HOST")
+	c.Port = os.Getenv("DB_PORT")
+	c.DBName = os.Getenv("DB_NAME")
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", c.User, c.Password, c.Host, c.Port, c.DBName)
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- types/types.go
+++ types/types.go
@@ -9,10 +9,10 @@ type User struct {
 type Users []User
 
 type DatabaseConnection struct {
-	Host     string `env:"DB_HOST"`
-	User     string `env:"DB_USER" envDefault:"root"`
-	Password string `env:"DB_PASS"`
-	Port     int    `env:"DB_PORT" envDefault:"5432"`
-	DBName   string `env:"DB_NAME" envDefault:"Workout"`
+	Host     string 
+	User     string 
+	Password string 
+	Port     string   
+	DBName   string 
 }
 
